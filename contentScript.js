7a1f53d6b31eceb2b4925963abf669dd721db925
Adesh Atole <adesh.atole@practo.com>
2019-11-07 20:04:06 +0530

    Add copy button
    - Works on list view
    - Works on standalone view

---
+++ contentScript.js
@@ -0,0 +1,74 @@
+function copyStringToClipboard (str) {
+    if(str === undefined || str === null || !str.trim()){
+        return
+    }
+    var el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+function isJira(){
+    return document.getElementsByTagName("body")[0].getAttribute("id") === 'jira'
+}
+
+function getButtonParent(){
+    const idContainers = document.getElementsByClassName('BreadcrumbsItem__BreadcrumbsItemElement-sc-1hh8yo5-0');
+    if(idContainers && idContainers.length){
+        return idContainers[idContainers.length - 1]
+    }
+}
+
+function refresh(){
+    if(!isJira())
+    return;
+    
+
+    
+    const copyButton = document.createElement('button');
+    copyButton.classList = ['copy-jira-id-button']
+    copyButton.textContent='COPY JIRA ID'
+    const pathElements = window.location.pathname.split('/');
+    let jiraId = '';
+    if(pathElements.length >= 3){
+        jiraId = pathElements[2];
+    }
+    
+    copyButton.onclick = () => copyStringToClipboard(jiraId);
+    const buttonParent = getButtonParent();
+    if(buttonParent){
+        let existingButtons = document.getElementsByClassName('copy-jira-id-button');
+    
+        for(button of existingButtons){
+            button.remove();
+        }
+        buttonParent.append(copyButton)
+    }else{
+        console.log('Copy JIRA ID => Something went wrong. Report to https://github.com/AdeshAtole/copy-jira-id/issues/new' )
+    }
+}
+
+console.log('Hello from \'Copy JIRA ID\'')
+refresh()
+
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if (!mutation.addedNodes) {
+            return;
+        }
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+            if (mutation.addedNodes[i].classList.contains("BreadcrumbsItem__BreadcrumbsItemElement-sc-1hh8yo5-0")) {
+                refresh()
+            }
+        }
+    });
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
