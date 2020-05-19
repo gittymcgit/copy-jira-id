5d56911f68fe7465a1b1adce271125565f00c79c
Adesh Atole <adesh.atole@practo.com>
2020-05-19 11:21:02 +0530

    Fix class check

--- contentScript.js
+++ contentScript.js
@@ -1,54 +1,70 @@
-function copyStringToClipboard (str) {
-    if(str === undefined || str === null || !str.trim()){
+function copyStringToClipboard(str) {
+    if (str === undefined || str === null || !str.trim()) {
         return
     }
     var el = document.createElement('textarea');
     el.value = str;
     el.setAttribute('readonly', '');
-    el.style = {position: 'absolute', left: '-9999px'};
+    el.style = { position: 'absolute', left: '-9999px' };
     document.body.appendChild(el);
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
 }
 
-function isJira(){
+function isJira() {
     return document.getElementsByTagName("body")[0].getAttribute("id") === 'jira'
 }
 
-function getButtonParent(){
-    const idContainers = document.getElementsByClassName('BreadcrumbsItem__BreadcrumbsItemElement-sc-1hh8yo5-0');
-    if(idContainers && idContainers.length){
-        return idContainers[idContainers.length - 1]
+function getButtonParent() {
+    let idContainers = document.getElementsByClassName('aui-nav-breadcrumbs');
+    let ui_version = 1;
+    if (!idContainers || !idContainers.length) {
+        idContainers = document.getElementsByClassName('BreadcrumbsItem__BreadcrumbsItemElement-sc-1hh8yo5-0');
+        ui_version = 2;
+    }
+
+    if (idContainers && idContainers.length) {
+
+        switch (ui_version) {
+            case 1:
+                const containers = idContainers[idContainers.length - 1].getElementsByTagName('li');
+                if (containers && containers.length) {
+                    return containers[0];
+                }
+                break;
+            case 2:
+                return idContainers[idContainers.length - 1]
+        }
     }
 }
 
-function refresh(){
-    if(!isJira())
-    return;
-    
+function refresh() {
+    if (!isJira())
+        return;
 
-    
+
+
     const copyButton = document.createElement('button');
     copyButton.classList = ['copy-jira-id-button']
-    copyButton.textContent='COPY JIRA ID'
+    copyButton.textContent = 'COPY JIRA ID'
     const pathElements = window.location.pathname.split('/');
     let jiraId = '';
-    if(pathElements.length >= 3){
+    if (pathElements.length >= 3) {
         jiraId = pathElements[2];
     }
-    
+
     copyButton.onclick = () => copyStringToClipboard(jiraId);
     const buttonParent = getButtonParent();
-    if(buttonParent){
+    if (buttonParent) {
         let existingButtons = document.getElementsByClassName('copy-jira-id-button');
-    
-        for(button of existingButtons){
+
+        for (button of existingButtons) {
             button.remove();
         }
         buttonParent.append(copyButton)
-    }else{
-        console.log('Copy JIRA ID => Something went wrong. Report to https://github.com/AdeshAtole/copy-jira-id/issues/new' )
+    } else {
+        console.log('Copy JIRA ID => Something went wrong. Report to https://github.com/AdeshAtole/copy-jira-id/issues/new')
     }
 }
 
@@ -61,7 +77,12 @@ var observer = new MutationObserver(function (mutations) {
             return;
         }
         for (var i = 0; i < mutation.addedNodes.length; i++) {
-            if (mutation.addedNodes[i].classList.contains("BreadcrumbsItem__BreadcrumbsItemElement-sc-1hh8yo5-0")) {
+            if (mutation.addedNodes[i].classList &&
+                (
+                    mutation.addedNodes[i].classList.contains("BreadcrumbsItem__BreadcrumbsItemElement-sc-1hh8yo5-0") ||
+                    mutation.addedNodes[i].classList.contains("Droplist-sc-1z05y4v-0")
+                )
+            ) {
                 refresh()
             }
         }
